d20bcbc05d6e9724ccaea5e58254fd0ee9d581e2
Bianca Olea <bolea@purdue.edu>
2024-03-28 11:33:46 -0400

    Startercode of User

--- User.java
+++ User.java
@@ -1,2 +1,37 @@
 package PACKAGE_NAME;public class User {
+    private int id; //internal number of user
+    private String username;
+    private String password;
+
+    public User(int id, String username, String password) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+    }
+
+    public User() {
+        this.id = 0;
+        this.username = null;
+        this.password = null;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword() {
+        this.password = password;
+    }
 }
