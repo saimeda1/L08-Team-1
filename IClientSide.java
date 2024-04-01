55cf6d426ccce420eee6f417f6190400e58c0270
Sai Meda <40012349+saimeda1@users.noreply.github.com>
2024-04-01 13:12:08 -0400

    Update IClientSide.java

--- IClientSide.java
+++ IClientSide.java
@@ -1,2 +1,8 @@
-package PACKAGE_NAME;public interface IClientSide {
+/**
+ * @author Chenjun Zhou, Xinan Qin, Sai Meda, Bianca Olea
+ * @version Apr.1
+ * This is for group project phrase 2
+ */
+public interface IClientSide {
+    //Interface for the client side of the social media application and GUI
 }
