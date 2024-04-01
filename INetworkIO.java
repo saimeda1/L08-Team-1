7bc59bb515915ba1282eedd292e47dc2e2e34392
Sai Meda <40012349+saimeda1@users.noreply.github.com>
2024-04-01 13:13:54 -0400

    Update INetworkIO.java

--- INetworkIO.java
+++ INetworkIO.java
@@ -1,2 +1,8 @@
-package PACKAGE_NAME;public interface INetworkIO {
+/**
+ * @author Chenjun Zhou, Xinan Qin, Sai Meda, Bianca Olea
+ * @version Apr.1
+ * This is for group project phase 2
+ */
+public interface INetworkIO {
+    // Interface for the network I/O of the social media application
 }
