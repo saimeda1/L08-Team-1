38fa31af10182d6bfb45d037b3ce68b0b51771d5
Sai Meda <40012349+saimeda1@users.noreply.github.com>
2024-04-01 13:14:49 -0400

    Update IUserDatabase.java

--- IUserDatabase.java
+++ IUserDatabase.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public interface IUserDatabase {
+import java.util.ArrayList;
+/**
+ * @author Chenjun Zhou, Xinan Qin, Sai Meda, Bianca Olea
+ * @version Apr.1
+ * This is for group project phase 1
+ */
+public interface IUserDatabase {
+    boolean signUp(User user);
+    ArrayList<Post> getPosts();
+    void setPosts();
+    boolean addComment(Comment comment, Post post);
+    boolean logIn(User user);
+    User searchUser(String search);
 }
