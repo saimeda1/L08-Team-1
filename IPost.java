bf463ff45624407f914f69b04bf4646e2ad5ada1
Sai Meda <40012349+saimeda1@users.noreply.github.com>
2024-04-01 13:14:17 -0400

    Update IPost.java

--- IPost.java
+++ IPost.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public interface IPost {
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+/**
+ * @author Chenjun Zhou, Xinan Qin, Sai Meda, Bianca Olea
+ * @version Apr.1
+ * This is for group project phase 1
+ */
+public interface IPost {
+    boolean isHide();
+    void setHide(boolean hide);
+    int getId();
+    String getContent();
+    void setContent(String content);
+    User getAuthor();
+    void setAuthor(User author);
+    LocalDateTime getTime();
+    void setTime(LocalDateTime time);
+    ArrayList<Comment> getComments();
+    void setComments(ArrayList<Comment> comments);
+    int getLikes();
+    void setLikes(int likes);
+    int getDislikes();
+    void setDislikes(int dislikes);
+    void upVote();
+    void downVote();
+    String toString();
 }
