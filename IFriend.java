7fb9cea60adce59b7b0fdc6776c5824ba684cb6c
Sai Meda <40012349+saimeda1@users.noreply.github.com>
2024-04-01 13:12:58 -0400

    Update IFriend.java

--- IFriend.java
+++ IFriend.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public interface IFriend {
+import java.util.ArrayList;
+/**
+ * @author Chenjun Zhou, Xinan Qin, Sai Meda, Bianca Olea
+ * @version Apr.1
+ * This is for group project phrase 1
+ */
+public interface IFriend {
+    boolean isBlock();
+    void setBlock(boolean block);
+    ArrayList<Post> getFriendPost();
+    boolean upVotePost(Post post);
+    boolean downVotePost(Post post);
+    void addPost(Post post);
 }
