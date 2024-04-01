3d77d5afbee30a2a74b7e9bf60141fa1fc3697f2
Sai Meda <40012349+saimeda1@users.noreply.github.com>
2024-04-01 13:12:44 -0400

    Update ICommentable.java

--- ICommentable.java
+++ ICommentable.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public interface ICommentable {
+/**
+ * @author Chenjun Zhou, Xinan Qin, Sai Meda, Bianca Olea
+ * @version Apr.1
+ * This is for group project phrase 1
+ */
+public interface ICommentable {
+    int getId();
+    int getLikes();
+    int getDislikes();
+    void setLikes(int likes);
+    void setDislikes(int dislikes);
+    void upVote();
+    void downVote();
+    String toString();
 }
