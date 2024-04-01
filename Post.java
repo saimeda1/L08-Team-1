91db28ef2274b689a625ad832d97c4be43744de2
zcj0125 <116486681+zcj0125@users.noreply.github.com>
2024-04-01 02:12:53 -0400

    Update Post.java

--- Post.java
+++ Post.java
@@ -1,29 +1,39 @@
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 
-public class Post{
-    private int id = 0;
+public class Post {
+    private static int nextId = 1;
+    private int id;
     private String content;
     private User author;
     private LocalDateTime time;
     private ArrayList<Comment> comments = new ArrayList<>();
     private int likes;
     private int dislikes;
+    private boolean hide;
 
 
-    public Post(String content, User author) {
+    public Post(String content, User author, boolean hide) {
+        this.id = nextId++;
         this.content = content;
         this.time = LocalDateTime.now();
         this.likes = 0;
         this.dislikes = 0;
         this.author = author;
-        id++;
+        this.hide = hide;
+    }
+
+    public boolean isHide() {
+        return hide;
+    }
+
+    public void setHide(boolean hide) {
+        this.hide = hide;
     }
 
     public int getId() {
         return id;
     }
-
 
 
     public String getContent() {
@@ -33,9 +43,11 @@ public class Post{
     public void setContent(String content) {
         this.content = content;
     }
+
     public User getAuthor() {
         return author;
     }
+
     public void setAuthor(User author) {
         this.author = author;
     }
@@ -59,22 +71,38 @@ public class Post{
     public int getLikes() {
         return likes;
     }
+
     public void setLikes(int likes) {
         this.likes = likes;
     }
+
     public int getDislikes() {
         return dislikes;
     }
+
     public void setDislikes(int dislikes) {
         this.dislikes = dislikes;
     }
-    public String toString(){
+
+    public void upVote() {
+        likes++;
+    }
+
+    public void downVote() {
+        dislikes++;
+    }
+
+    public String toString() {
         String result = "";
-        for (int i = 0; i < comments.size(); i++){
+        for (int i = 0; i < comments.size(); i++) {
             result += comments.get(i).toString() + "\n";
         }
-        return  "PostID: " + id + "\n" +
+        if (hide) {
+            return "This post has been hidden\n";
+        }
+        return "PostID: " + id + "\n" +
                 "Post: " + content + "\n" + "by " + author.getUsername() + "\n" + "at " +
-                time + "\n" + "likes: " + likes + "\n" + "dislikes: " + dislikes + "\n" + result;
+                time + "\n" + "likes: " + likes + "\n" + "dislikes: " + dislikes + "\n" +
+                result;
     }
 }
